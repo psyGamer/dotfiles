4a727d912e9f1c295eb15d507f4cd24d75fd0a90
psyGamer <psygamer21@gmail.com>
2022-05-26 01:03:30 +0200

    Cleaned up Wibar config

--- .config/awesome/bars.lua
+++ .config/awesome/bars.lua
@@ -1,11 +1,11 @@
 --[[	Main configuration
-                                            __        ____  __ 
-  __ ___      _____  ___  ___  _ __ ___   __\ \      / /  \/  |
+																						__				____	__ 
+	__ ___			_____  ___	___  _ __ ___		__\ \			 / /	\/	|
  / _` \ \ /\ / / _ \/ __|/ _ \| '_ ` _ \ / _ \ \ /\ / /| |\/| |
-| (_| |\ V  V /  __/\__ \ (_) | | | | | |  __/\ V  V / | |  | |
- \__,_| \_/\_/ \___||___/\___/|_| |_| |_|\___| \_/\_/  |_|  |_|
+| (_| |\ V	V /  __/\__ \ (_) | | | | | |  __/\ V  V / | |	| |
+ \__,_| \_/\_/ \___||___/\___/|_| |_| |_|\___| \_/\_/  |_|	|_|
 
-	Author: 	psyGamer
+	Author:		psyGamer
 	Repository: https://github.com/psyGamer/dotfiles
 ]]--
 
@@ -23,62 +23,55 @@ local beautiful = require("beautiful")
 
 mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
 									 menu = mymainmenu })
-mykeyboardlayout = awful.widget.keyboardlayout()
 
 -- Create a textclock widget
 mytextclock = wibox.widget.textclock()
 
 -- Create a wibox for each screen and add it
 local taglist_buttons = gears.table.join(
-					awful.button({ }, 1, function(t) t:view_only() end),
-					awful.button({ modkey }, 1, function(t)
-											  if client.focus then
-												  client.focus:move_to_tag(t)
-											  end
-										  end),
-					awful.button({ }, 3, awful.tag.viewtoggle),
-					awful.button({ modkey }, 3, function(t)
-											  if client.focus then
-												  client.focus:toggle_tag(t)
-											  end
-										  end),
-					awful.button({ }, 4, function(t) awful.tag.viewnext(t.screen) end),
-					awful.button({ }, 5, function(t) awful.tag.viewprev(t.screen) end)
+	awful.button({ }, 1, function(t) t:view_only() end),
+	awful.button({ modkey }, 1, function(t)
+		if client.focus then
+			client.focus:move_to_tag(t)
+		end
+	end),
+	awful.button({ }, 3, awful.tag.viewtoggle),
+	awful.button({ modkey }, 3, function(t)
+		if client.focus then
+			client.focus:toggle_tag(t)
+		end
+	end),
+	awful.button({ }, 4, function(t) awful.tag.viewnext(t.screen) end),
+	awful.button({ }, 5, function(t) awful.tag.viewprev(t.screen) end)
 				)
 
 local tasklist_buttons = gears.table.join(
-					 awful.button({ }, 1, function (c)
-											  if c == client.focus then
-												  c.minimized = true
-											  else
-												  c:emit_signal(
-													  "request::activate",
-													  "tasklist",
-													  {raise = true}
-												  )
-											  end
-										  end),
-					 awful.button({ }, 3, function()
-											  awful.menu.client_list({ theme = { width = 250 } })
-										  end),
-					 awful.button({ }, 4, function ()
-											  awful.client.focus.byidx(1)
-										  end),
-					 awful.button({ }, 5, function ()
-											  awful.client.focus.byidx(-1)
-										  end))
+	awful.button({ }, 1, function (c)
+		if c == client.focus then
+			c.minimized = true
+		else
+			c:emit_signal(
+				"request::activate",
+				"tasklist",
+				{raise = true}
+			)
+		end
+	end),
+	awful.button({ }, 3, function()	awful.menu.client_list({ theme = { width = 250 } })	end),
+	awful.button({ }, 4, function()	awful.client.focus.byidx( 1)	end),
+	awful.button({ }, 5, function()	awful.client.focus.byidx(-1) end))
 
-return function(s)
+awful.screen.connect_for_each_screen(function(s)
 	-- Create a promptbox for each screen
 	s.mypromptbox = awful.widget.prompt()
 	-- Create an imagebox widget which will contain an icon indicating which layout we're using.
 	-- We need one layoutbox per screen.
 	s.mylayoutbox = awful.widget.layoutbox(s)
 	s.mylayoutbox:buttons(gears.table.join(
-						   awful.button({ }, 1, function () awful.layout.inc( 1) end),
-						   awful.button({ }, 3, function () awful.layout.inc(-1) end),
-						   awful.button({ }, 4, function () awful.layout.inc( 1) end),
-						   awful.button({ }, 5, function () awful.layout.inc(-1) end)))
+		awful.button({ }, 1, function () awful.layout.inc( 1) end),
+		awful.button({ }, 3, function () awful.layout.inc(-1) end),
+		awful.button({ }, 4, function () awful.layout.inc( 1) end),
+		awful.button({ }, 5, function () awful.layout.inc(-1) end)))
 	-- Create a taglist widget
 	s.mytaglist = awful.widget.taglist {
 		screen	= s,
@@ -100,17 +93,14 @@ return function(s)
 		layout = wibox.layout.align.horizontal,
 		{ -- Left widgets
 			layout = wibox.layout.fixed.horizontal,
-			mylauncher,
 			s.mytaglist,
-			s.mypromptbox,
 		},
-		s.mytasklist, -- Middle widget
+		nil, -- Middle widget
 		{ -- Right widgets
 			layout = wibox.layout.fixed.horizontal,
-			mykeyboardlayout,
 			wibox.widget.systray(),
 			mytextclock,
 			s.mylayoutbox,
 		},
 	}
-end
+end)
--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -10,10 +10,6 @@
 ]]--
 
 -- {{{ Library definitions
--- If LuaRocks is installed, make sure that packages installed through it are
--- found (e.g. lgi). If LuaRocks is not installed, do nothing.    
-pcall(require, "luarocks.loader")
-
 -- Standard awesome library
 local gears = require("gears")
 local awful = require("awful")
@@ -31,8 +27,8 @@ local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
 
 -- Modules
-local bindings   = require("bindings")
-local createBars = require("bars")
+local bindings = require("bindings")
+                 require("bars")
 
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
@@ -138,9 +134,6 @@ awful.screen.connect_for_each_screen(function(s)
 
 	-- Each screen has its own tag table.
 	awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
-
-	-- Setup bars
-	createBars(s)
 end)
 -- }}}
 
