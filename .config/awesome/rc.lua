5d28ede11b0f31f1996aca9400e0e6ee168f6eac
psyGamer <psygamer21@gmail.com>
2022-05-26 16:32:58 +0200

    Removed --experimental-backends of picom
    It caused issues when using nested awesomeWM instances

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -277,7 +277,8 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 awful.spawn.with_shell("systemctl --user mask --runtime plasma-kglobalaccel.service")
 
 -- Compositing & Wallpaper
-awful.spawn.with_shell("picom --experimental-backends")
+--awful.spawn.with_shell("picom --experimental-backends")
+awful.spawn.with_shell("picom")
 awful.spawn.with_shell("nitrogen --restore")
 
 awful.spawn.with_shell("~/.scripts/startup.sh") -- TODO Remove personal stuff
