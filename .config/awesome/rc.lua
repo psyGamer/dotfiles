294239b30f8304059d3dd86fdc4a9fb341597258
psyGamer <psygamer21@gmail.com>
2022-05-26 23:07:42 +0200

    Extacted window rules into seperate file

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -29,6 +29,7 @@ local hotkeys_popup = require("awful.hotkeys_popup")
 -- Modules
 local global   = require("global")
 				 require("config.keys")
+				 require("config.rules")
                  --require("bars")
 
 -- Enable hotkeys help widget for VIM and other apps
@@ -139,62 +140,6 @@ awful.screen.connect_for_each_screen(function(s)
           beautiful.at_screen_connect(s)
         end
 end)
--- }}}
-
--- {{{ Rules
--- Rules to apply to new clients (through the "manage" signal).
-awful.rules.rules = {
-	-- All clients will match this rule.
-	{ rule = { },
-	  properties = { border_width = beautiful.border_width,
-					 border_color = beautiful.border_normal,
-					 focus = awful.client.focus.filter,
-					 raise = true,
-					 screen = awful.screen.preferred,
-					 placement = awful.placement.no_overlap+awful.placement.no_offscreen
-	 }
-	},
-
-	-- Floating clients.
-	{ rule_any = {
-		instance = {
-		  "DTA",  -- Firefox addon DownThemAll.
-		  "copyq",	-- Includes session name in class.
-		  "pinentry",
-		},
-		class = {
-		  "Arandr",
-		  "Blueman-manager",
-		  "Gpick",
-		  "Kruler",
-		  "MessageWin",  -- kalarm.
-		  "Sxiv",
-		  "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
-		  "Wpa_gui",
-		  "veromix",
-		  "xtightvncviewer"},
-
-		-- Note that the name property shown in xprop might be set slightly after creation of the client
-		-- and the name shown there might not match defined rules here.
-		name = {
-		  "Event Tester",  -- xev.
-		},
-		role = {
-		  "AlarmWindow",  -- Thunderbird's calendar.
-		  "ConfigManager",	-- Thunderbird's about:config.
-		  "pop-up",		  -- e.g. Google Chrome's (detached) Developer Tools.
-		}
-	  }, properties = { floating = true }},
-
-	-- Add titlebars to normal clients and dialogs
-	{ rule_any = {type = { "normal", "dialog" }
-	  }, properties = { titlebars_enabled = true }
-	},
-
-	-- Set Firefox to always map on the tag named "2" on screen 1.
-	-- { rule = { class = "Firefox" },
-	--	 properties = { screen = 1, tag = "2" } },
-}
 -- }}}
 
 -- {{{ Signals
