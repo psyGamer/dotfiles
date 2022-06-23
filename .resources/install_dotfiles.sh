356a78943dfd497000ca0ab5a042a41a5ceb7af9
psyGamer <psygamer21@gmail.com>
2022-06-23 18:28:51 +0200

    Fixed install scripts

--- .resources/install_dotfiles.sh
+++ .resources/install_dotfiles.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/bash
 
-SCRIPT_DIR=$(cd `dirname $0` && pwd)
+SCRIPT_DIR=$(cd `dirname $0`/.. && pwd)
 
 install_files() {
-	for file in $(ls -la $SCRIPT_DIR/$1)
-	do
-		BASENAME=$(basename $file)
-		if test -e ~/$1/$BASENAME
-			echo "~/.config/$BASENAME already exists! Creating backup."
-			mv ~/$1/$BASENAME ~/$1/$BASENAME.backup
-		fi
-	
-		cp -r $SCRIPT_DIR/$1/$BASENAME ~/$1/$BASENAME
-		echo "Successfully install ~/.config/$BASENAME!"
-	done
+    for file in $SCRIPT_DIR/$1/*; do
+        BASENAME=$(basename $file)
+        if test -e ~/$1/$BASENAME; then
+            echo "~/$1/$BASENAME already exists! Creating backup."
+            mv ~/$1/$BASENAME ~/$1/$BASENAME.backup
+        fi
+
+        cp -r $SCRIPT_DIR/$1/$BASENAME ~/$1/$BASENAME
+        echo "Successfully install ~/$1/$BASENAME!"
+    done
 }
 
 install_files .resources
@@ -27,3 +26,22 @@ chmod +x install_cursors.sh
 ./install_cursors.sh
 
 echo "Successfully installed dotfiles!"
+
+echo
+echo "Installing required packages..."
+sudo pacman -S --needed --yes - < ~/.resources/pacman_packages
+
+if ! pacman -Qs paru > /dev/null; then
+    echo
+    echo "Installing paru..."
+    sudo pacman -S --needed --yes base-devel
+    git clone https://aur.archlinux.org/paru.git /tmp/paru-git
+    pushd /tmp/paru-git
+    makepkg -si --noconfirm
+    popd
+    rm -rd /tmp/paru-git
+fi
+
+echo
+echo "Installing required AUR packages..."
+sudo paru -S --needed --yes - < ~/.resources/aur_packages
